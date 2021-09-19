b203edfcc445f55617f90a1130fbff8fcfca2c79
Dávid El-Saig <david.el-saig@lombiq.com>
2021-09-19 19:36:35 +0200

    Code cleanup & bugfix.

--- src/ConstantGenerator/Extensions.cs
+++ src/ConstantGenerator/Extensions.cs
@@ -4,13 +4,6 @@ namespace System.Collections.Generic
 {
     public static class Extensions
     {
-        public static TValue GetMaybe<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default)
-        {
-            return dictionary.TryGetValue(key, out var result)
-                ? result
-                : defaultValue;
-        }
-
         /// <summary>
         /// Converts snake_case, kebab-case and scene.case to PascalCase.
         /// </summary>
@@ -19,6 +12,9 @@ namespace System.Collections.Generic
         public static string ToPascalCase(this string text) =>
             string.Join(
                 string.Empty,
-                text.Split('-', '_', '.').Select(word => word[..1].ToUpper()));
+                text
+                    .Split('-', '_', '.')
+                    .Where(word => word.Length >= 1)
+                    .Select(word => word[..1].ToUpper() + word[1..]));
     }
 }
